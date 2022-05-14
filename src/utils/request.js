9ea3a265d3d0dca6604889729a60934bfc86d1f8
ChandrakantPal <sanjay161097@gmail.com>
2022-05-14 23:29:56 +0530

    requests export const

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
-const API_KEY = process.env.REACT_APP_NETFLIX_CLONE_API_KEY;
+const API_KEY = process.env.REACT_APP_NETFLIX_CLONE_API_KEY
 
-export default {
+export const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -9,4 +9,4 @@ export default {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-};
+}
