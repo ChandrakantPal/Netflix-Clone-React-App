485745ee3fcfa1c55f899a3d8ba8bae4a9437d18
ChandrakantPal <sanjay161097@gmail.com>
2022-05-14 23:19:42 +0530

    header

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,34 +1,28 @@
-import React, { useState, useEffect } from "react";
-import "./Nav.css";
+import React, { useState, useEffect } from 'react'
+import './Nav.css'
+import netflixLogo from '../../assets/netflix.svg'
+import avator from '../../assets/avator.png'
 
 function Nav() {
-  const [show, handleShow] = useState(false);
+  const [show, handleShow] = useState(false)
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
       if (window.scrollY > 100) {
-        handleShow(true);
-      } else handleShow(false);
-    });
+        handleShow(true)
+      } else handleShow(false)
+    })
     return () => {
-      window.removeEventListener("scroll");
-    };
-  }, []);
+      window.removeEventListener('scroll')
+    }
+  }, [])
 
   return (
-    <nav className={`nav ${show && "nav__black"}`}>
-      <img
-        className="nav__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
-        alt="Netflix Logo"
-      />
-      <img
-        className="nav__avatar"
-        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-        alt="avatar"
-      />
+    <nav className={`nav ${show && 'nav__black'}`}>
+      <img className="nav__logo" src={netflixLogo} alt="Netflix Logo" />
+      <img className="nav__avatar" src={avator} alt="avatar" />
     </nav>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
